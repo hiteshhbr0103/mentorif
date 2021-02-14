9c16bc76303697ebda0d836948ad6b249e2dfcfb
bhatia <hitesh.bhatia@student.griffith.ie>
2021-02-14 00:43:58 +0000

    Final commit

---
+++ Task2.py
@@ -0,0 +1,14 @@
+from Task1 import Task as tsk
+number = input('Enter number or string :')
+if(tsk.is_digit(number)):
+    newNum = int(number)
+    res = tsk.numVal(newNum)
+    if(res=="True"):
+        print("The Given number is Even")
+    else:
+        print("The given number is Odd")
+else:
+    # task2
+    output = tsk.wordCount(number)
+    output = str(output)
+    print("Number of word count is " + output)
